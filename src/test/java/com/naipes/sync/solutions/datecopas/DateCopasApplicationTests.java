a371baf623b90e9e03ee319d9e7211ffd8f34a92
Eddie Dias <eddias.mazor@gmail.com>
2024-06-14 16:03:24 -0300

    First Commit

---
+++ src/test/java/com/naipes/sync/solutions/datecopas/DateCopasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naipes.sync.solutions.datecopas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateCopasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
